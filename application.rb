03f8524e1dfeb231fa5e1ecbf99bb927e6f5f5c7
Trevor Grandle <trevor.grandle@synchronyfinancial.com>
2016-06-15 13:32:08 -0400

    poor man\'s way to run a migration

--- application.rb
+++ application.rb
@@ -157,3 +157,29 @@ helpers do
   end
 end
 
+#### tjg
+get '/db' do
+  require 'rubygems'
+  require 'bundler/setup'
+
+  require 'songkick/oauth2/provider'
+  require 'active_record'
+  require File.expand_path('../models/connection', __FILE__)
+
+  ActiveRecord::Schema.define do |version|
+    create_table :users, :force => true do |t|
+      t.timestamps
+      t.string :username
+    end
+
+    create_table :notes, :force => true do |t|
+      t.timestamps
+      t.belongs_to :user
+      t.string     :title
+      t.text       :body
+    end
+  end
+
+  Songkick::OAuth2::Model::Schema.up
+
+end  
